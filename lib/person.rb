9e2599cad7cbb19398811884a6d625be9cadd312
Loise <loise.kimani@student.moringaschool.com>
2023-02-13 13:13:05 +0300

    Done again

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,29 @@
 class Person
-   attr_accessor :name, :job
-  end
+
+   # setter method
+   def name=(name)
+     @name = name
+   end
+
+   def breed=(breed)
+      @breed = breed
+   end
+
+   def job=(job)
+      @job = job
+   end
+ 
+   # getter method
+   def name
+     @name
+   end
+ 
+   def job
+      @job
+   end
+
+   def breed
+      @breed
+   end
+
+ end
